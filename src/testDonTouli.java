54131328160a5302e1ee6a53a1dc073d0d848509
Samir Toularhmine <samir.toularhmine@etu.univ-orleans.fr>
2019-03-11 16:46:20 +0100

    Modification projet

---
+++ src/testDonTouli.java
@@ -0,0 +1,23 @@
+public class test {
+
+    public static void main(String[] args) {
+        FileParser fp = new FileParser("arbo");
+        fp.parserFichier();
+        /*try {
+            ArbreFichiers root = new ArbreFichiers(null, null, null, null, "root");
+            System.out.println(root);
+            ArbreFichiers sRoot = new ArbreFichiers(root, null, null, null, "sRoot");
+            root.setPremierFils(sRoot);
+            System.out.println(sRoot);
+            ArbreFichiers s2root = new ArbreFichiers(root,null,sRoot,null,"s2root");
+            ArbreFichiers sRootSame = new ArbreFichiers(root, null, null, null, "sR1oot");
+            sRoot.setFrereDroit(s2root);
+            root.addNode(sRootSame);
+            System.out.println(root);
+            System.out.println("sRootSame = "+ sRootSame);
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }*/
+    }
+
+}
