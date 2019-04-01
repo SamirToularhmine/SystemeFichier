6f8ebc3575eed022b074aa675296c072c8631608
Vali00 <45363371+Vali00@users.noreply.github.com>
2019-04-01 22:27:34 +0200

    commande find

--- src/commandes/Commandes.java
+++ src/commandes/Commandes.java
@@ -30,6 +30,7 @@ public class Commandes {
         commandes.put(MKFILE, new Mkfile());
         commandes.put(MKDIR, new Mkdir());
         commandes.put(LESS, new Less());
+        commandes.put(FIND, new Find());
     }
 
     public static Map<String,Commande> importCmd(){
