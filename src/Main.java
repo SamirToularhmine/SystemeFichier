6d9c60a81283aecf90359b3894727c05d284dbba
Samir Toularhmine <samir.toularhmine@etu.univ-orleans.fr>
2019-05-03 23:57:05 +0200

    Ajout du cas sans fichier

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import systeme_fichier.SystemeFichier;
 public class Main {
 
     public static void main(String[] args) {
-        SystemeFichier sf = new SystemeFichier("../arbo");
+        //SystemeFichier sf = new SystemeFichier("arbo");
+        SystemeFichier sf = new SystemeFichier();
     }
 }
